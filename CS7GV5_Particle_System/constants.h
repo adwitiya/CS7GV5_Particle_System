76f28b30edff97dbabd3cff58c8728df6fea75ea
adwitiya <chakraad@tcd.ie>
2018-02-06 00:47:56 +0000

    changed max velocity.

--- CS7GV5_Particle_System/constants.h
+++ CS7GV5_Particle_System/constants.h
@@ -7,7 +7,7 @@ const int NUMBER_OF_PARTICLES = 333;
 const float LENGTH = 100.0;
 
 const int MIN_INIT_VELOCITY = 10;
-const int MAX_INIT_VELOCITY = 100;
+const int MAX_INIT_VELOCITY = 50;
 const int MAX_VELOCITY = 200;
 
 const int MIN_MASS = 2;
