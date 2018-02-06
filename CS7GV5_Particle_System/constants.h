f5b721c7286054975636a41294db8ccecc0fdbf0
adwitiya <chakraad@tcd.ie>
2018-02-06 02:00:38 +0000

    modified contants.

--- CS7GV5_Particle_System/constants.h
+++ CS7GV5_Particle_System/constants.h
@@ -4,15 +4,15 @@
 
 const float DELTA = 1;
 const int NUMBER_OF_PARTICLES = 333;
-const float LENGTH = 100.0;
+const float LENGTH = 110.0;
 
 const int MIN_INIT_VELOCITY = 10;
 const int MAX_INIT_VELOCITY = 50;
 const int MAX_VELOCITY = 200;
 
-const int MIN_MASS = 2;
+const int MIN_MASS = 8;
 const int MAX_MASS = 10;
 
 const int FORCE_MAG = 2000;
 
-const int MAX_PARTICLES = 10000;
+const int MAX_PARTICLES = 1000;
